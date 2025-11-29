9a00796fe163d1c7f569388cf854045258059c1e
Dolly Yap <douleiamaeyap@gmail.com>
2025-11-29 19:34:36 +0800

    reformatted raw dataset

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # PRE-PROCESSING
 # when training images have not been segmented for feature extraction
-DATASET_UNCROPPED = "./dataset/train/" #change to class
-EXPORT_CROPPED = "./dataset/train/images/" #change to class
+DATASET_UNCROPPED = "./raw_dataset/" 
+EXPORT_CROPPED = "./dataset/cropped/" 
 RESIZE = 244
 
 # when scaler is not fitted
-TRAIN_ROOT = "./dataset/train/images/"
+TRAIN_ROOT = "./dataset/train/"
--- main.py
+++ main.py
@@ -14,37 +14,37 @@ Prepare Dataset
 - feature extraction and scaler fitting
 '''
 # IMAGE SEGMENTATION OF DATASET - only run this section once
-# category = ['capsule', 'tablet']
-# ctr = 0
-# #Training dataset
-# for cat in category:
-#     data = Path(DATASET_UNCROPPED + f"{cat}/")
-#     output = Path(EXPORT_CROPPED + f"{cat}/")
-#     output.mkdir(parents=True, exist_ok=True)
+category = ['capsule', 'tablet']
+ctr = 0
+#Training dataset
+for cat in category:
+    data = Path(DATASET_UNCROPPED + f"{cat}/")
+    output = Path(EXPORT_CROPPED + f"{cat}/")
+    output.mkdir(parents=True, exist_ok=True)
 
-#     paths = list(data.rglob("*.[jpg]*"))
+    paths = list(data.rglob("*.[jpg]*"))
 
-#     if not paths:
-#         print(f"No images found in {data}")
-#         continue
+    if not paths:
+        print(f"No images found in {data}")
+        continue
     
-#     print(f"Processing category: {cat.upper()} ({len(paths)} images)")
-#     for path in tqdm(paths, desc=cat, unit="img"):
-#         img_seg = ImageSegmenter(cv2.imread(path))
-#         extracted, bboxes = img_seg.segment()
-#         if extracted:
-#             cv2.imwrite(EXPORT_CROPPED + f"{cat}/" + f"/capsule{ctr}.png", extracted[0]) #change to class
-#         ctr+=1
-# print("Segmentation complete")
+    print(f"Processing category: {cat.upper()} ({len(paths)} images)")
+    for path in tqdm(paths, desc=cat, unit="img"):
+        img_seg = ImageSegmenter(cv2.imread(path))
+        extracted, bboxes = img_seg.segment()
+        if extracted:
+            cv2.imwrite(EXPORT_CROPPED + f"{cat}/" + f"/capsule{ctr}.png", extracted[0]) #change to class
+        ctr+=1
+print("Segmentation complete")
 
 #FEATURE EXTRACTION AND SCALING
 # extractor = FeatureExtractor()
 # extractor.fit_and_save('features.npz')
 
 # train model
-data = np.load("features.npz")
-X = data['features']
-y = data['labels']
+# data = np.load("features.npz")
+# X = data['features']
+# y = data['labels']
 
 
 # test performance
